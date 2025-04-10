6666fd0b9bfe3473c1d82cdc9f8d6b1bea6a6b26
Samueljain26 <samueljain77@gmail.com>
2025-04-10 13:56:35 +0530

    [Samuel] ADD:parsing json file and printing the names of person with age>25.

---
+++ src/main/java/com/bridgelabz/practiceproblems/FilterJsonFile.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.practiceproblems;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FilterJsonFile {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode array = mapper.readTree(new FileReader("Age.json"));
+        for(JsonNode node: array){
+            int age = node.get("age").asInt();
+            if(age>25){
+                String name =node.get("name").asText();
+                System.out.println("Name: "+name + ", Age: "+ age);
+
+            }
+        }
+    }
+}
