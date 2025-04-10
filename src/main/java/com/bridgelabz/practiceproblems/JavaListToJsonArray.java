b858e29215e6524e39199362e46a02141f0ce6b6
Samueljain26 <samueljain77@gmail.com>
2025-04-10 13:46:47 +0530

    [Samuel] ADD:converting java list to json array.

---
+++ src/main/java/com/bridgelabz/practiceproblems/JavaListToJsonArray.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.practiceproblems;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.*;
+
+class User3 {
+    private String name;
+    private int id;
+
+    public User3(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class JavaListToJsonArray {
+    public static void main(String[] args) {
+        List<User3> list = new ArrayList<>();
+        list.add(new User3("Samuel",1));
+        list.add(new User3("B",2));
+        list.add(new User3("C",3));
+
+        try{
+            ObjectMapper mapper =new ObjectMapper();
+            String json = mapper.writeValueAsString(list);
+            System.out.println(json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
